9bf0f1be6c16811cba7e325d425bb5f2c9de6dc6
Anthonio Hagi Tobing <133083562+anthoniohagitobing@users.noreply.github.com>
2024-01-08 18:58:29 +0900

    feat: add allowed host

--- server/settings.py
+++ server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-y4ybm!faoq%b#0xaw)%r)20=n&@@7kq34b+ss$&33#le0tes$#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bookkeeper-server-405b5350d93b.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
